993411dcccb2f9851eeed2e9116d096553bf788f
Mirlan <mika.sootbaev1309@gmail.com>
2022-05-04 20:02:59 +0600

    Добавили модель болгов, вьюшку для отображения всех блогов

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def blog_list(request):
+	blogs = Blog.objects.all() # Запрашиваем из БД первый блог
+	context = {
+		'blogs': blogs,
+	}
+	return render(request, 'blogs/blog_list.html', context)
